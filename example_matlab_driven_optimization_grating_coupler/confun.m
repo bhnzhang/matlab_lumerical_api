b8549bfd36569ab3add19b40a7ea3525c547eec1
Bohan Zhang <bhnzhang@gmail.com>
2024-10-23 22:25:26 -0400

    Delete example_matlab_driven_optimization_grating_coupler directory

--- example_matlab_driven_optimization_grating_coupler/confun.m
+++
@@ -1,8 +0,0 @@
-function [c, ceq] = confun(x)
-
-% Nonlinear inequality constraints x(x_pos,theta,pitch,dc)
-global gap
-c = tan(x(2)*pi/180)*gap-x(1)*1e-5;
-
-% Nonlinear equality constraints
-ceq = [];
